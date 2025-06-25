d322087e7167adcbc207b44ede7847ccbb0efbfb
ABDERRAHMANEELMOTAOUAKKIL <97250011+ABDERRAHMANEELMOTAOUAKKIL@users.noreply.github.com>
2025-06-25 16:08:34 +0100

    new changes

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,26 +1,25 @@
 // src/data/projects.js
 import imageeci from '../images/Logo-eci-2.png';
-import Logo2 from '../images/Logo2.svg';
 import Logo3 from '../images/Logo-Water.png';
-import apf from "../images/apf.svg"
+// import apf from "../images/apf.svg"
 import imgdefault from "../images/default-image.webp"
 
 const projects = [
   {
     name: "Eci ",
     description: "An engineering consulting firm that works in various areas of sustainable development, including the environment, climate change, the green economy and financing, private sector development, and many others.",
-    link: "https://eci.yanvision.ma/en/",
+    link: "https://eci.ma/fr/",
     imgSrc: imageeci,
     tools: ["Wagtail", "Django", "PostgreSQL"]
 
     
   },
   {
-    name: "IronParts",
-    description: "E-comerce is a specialized e-commerce platform dedicated to providing a comprehensive range of car materials and accessories. Catering to both automotive enthusiasts and professionals, E-comerce offers an extensive selection of high-quality products designed to enhance vehicle performance, maintenance, and customization.",
-    link: "https://ironparts.yanvision.ma/",
-    imgSrc: Logo2,
-    tools: ["Wagtail", "Django", "PostgreSQL","Dashboard Quasar"]
+    name: "Awards Engagement ",
+    description: " Engagement Awards, which recognize companies and professionals committed to social, environmental, and societal responsibility. The platform offers information about the awards, eligibility criteria, registration steps, and deadlines. It allows users to submit their applications online, manage their submissions, and access support throughout the process. The awards highlight initiatives that make a real difference, encouraging organizations to showcase their positive impact. The site is available in French and is designed for simplicity, clarity, and user-friendly navigation, making it easy for companies to participate and gain recognition for their engagement efforts.",
+    link: "https://awardsengagement.fr/",
+    imgSrc: imgdefault,
+    tools: ["Wagtail", "Django", "PostgreSQL"]
 
   },
   {
@@ -32,21 +31,21 @@ const projects = [
 
   },
   {
-    name: "Opti",
-    description: "Opti provides comprehensive information about each product, including specifications, benefits, and usage tips. Educational resources such as blog posts and FAQs help customers stay informed about improvements.",
-    link: "https://www.opti.yanvision.ma/",
+    name: "Elym Academy",
+    description: "Elym provides sustainable solutions for eco-responsible event management. Their services include the rental of reusable tableware, waste sorting stations, and zero-waste logistics tailored for corporate events, festivals, and large gatherings. Elym supports clients in reducing their environmental footprint by offering practical, turnkey solutions that replace single-use items. The site highlights their mission, values, and impact, showcasing how events can be both professional and sustainable. With a clean, modern design, the platform allows visitors to learn about Elym’s services, view client testimonials, and easily request quotes or consultations.",
+    link: "https://elym.green/fr/",
     imgSrc: imgdefault,
     tools: ["Wagtail", "Django", "Docker"]
 
   },
-  {
-    name: "Apf",
-    description: "Apf provides comprehensive information about each product, including specifications, benefits, and usage tips. Educational resources such as blog posts and FAQs help customers stay informed about improvements.",
-    link: "https://apf.yanvision.ma/",
-    imgSrc: apf,
-    tools: ["Wagtail", "Django", "Docker"]
+  // {
+  //   name: "Apf",
+  //   description: "Apf provides comprehensive information about each product, including specifications, benefits, and usage tips. Educational resources such as blog posts and FAQs help customers stay informed about improvements.",
+  //   link: "https://apf.yanvision.ma/",
+  //   imgSrc: apf,
+  //   tools: ["Wagtail", "Django", "Docker"]
 
-  }
+  // }
 ];
 
   export default projects;
